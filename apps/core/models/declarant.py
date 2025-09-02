ce9f5448ca13a97d84021deeada7fc40de69594d
Honcharuk Bohdan <teofipol_sv@ukr.net>
2025-09-02 21:57:45 +0300

    Declarant -client class

--- apps/core/models/declarant.py
+++ apps/core/models/declarant.py
@@ -10,20 +10,5 @@ class Declarant(models.Model):
     work_post = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.surname} {self.name} {self.patronymic}'
-
-    
-# class Gender(models.IntegerChoices):
-#     NOT_SPECIFIED = 0, "Not specified"
-#     MALE = 1, "Male"
-#     FEMALE = 2, "Female"
-#     OTHER = 3, "Other"
-#
-# class Declarations(models.Model):
-#     id = models.AutoField(primary_key=True)
-#     surname = models.CharField(max_length=50)
-#     name = models.CharField(max_length=50)
-#     patronymic = models.CharField(max_length=50)
-#     email = models.EmailField()
-#     birthdate = models.DateField()
-#     gender = models.IntegerField(choices=Gender.choices)
+        return \
+            f'{self.surname} {self.name} {self.patronymic}'
