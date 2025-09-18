416488c2d508a091a6538128448181f64e5b213f
Honcharuk Bohdan <teofipol_sv@ukr.net>
2025-09-18 21:38:20 +0300

    Added declarants page, updated styles

---
+++ apps/core/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from apps.core.models import Declarant
+
+
+class DeclarantForm(forms.ModelForm):
+    class Meta:
+        model = Declarant
+        # 1
+        fields = '__all__'
+        exclude = ['created_at', 'updated_at', 'owner', 'user_declarant_id', 'api_id']
+        # 2
+        # fields = ['']
+        # widgets = {
+            # 'surname': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Прізвище'}),
+
+        # }
